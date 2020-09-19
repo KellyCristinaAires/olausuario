5dc0263eaf6db1b372eebd309e7b47339bc1a3af
KellyCristinaAires <k.woys@hotmail.com>
2020-09-18 21:56:29 -0300

    atividade ola usuario

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace olausuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+
+            //<---Inicio(Digite--->//
+            Console.Write("Digite Seu Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine($"Olá, {nome}!");
+            Console.WriteLine("PRESSIONE ENTER PARA SAIR");
+            Console.ReadKey();
+        }
+
+    }
+}
